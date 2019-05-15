dad84a12e5776ffc34a177b9b5ea6fd285ac8d63
Evgenii <vintumrage@ukr.net>
2019-05-15 17:37:13 +0300

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+class Diapason
+{
+private:
+	int left;
+	int right;
+public:
+	Diapason(int left, int right) {
+		this->left = left;
+		this->right = right;
+	};
+	~Diapason() {
+	};
+	void insert() {
+		cout << "Insert left range of edge: ";
+		cin >> left;
+		cout << "\nInsert right range of edge: ";
+		cin >> right;
+	}
+	int getLeft() {
+		return left;
+	};
+	int getRight() {
+		return right;
+	};
+
+
+	int findLength() {
+		return right - left;
+	};
+	bool checkForExist(int checkValue) {
+		if (checkValue >= left && checkValue <= right)return true;
+		else return false;
+	};
+
+	bool intersection(Diapason x) {
+		if (x.left >= left && x.left <= right || x.right >= left && x.right <= right) return true;
+		else return false;
+	};
+};
+
+int main()
+{
+	Diapason D1(-10, -3);
+	Diapason D2(-5, 4);
+	cout << "Length of diapason is " << D1.findLength() << endl;;
+	cout << D1.checkForExist(-10);
+	cout <<"\Intersection: "<<D1.intersection(D2) << endl;
+	return 0;
+}
+
+//1.«ДИАПАЗОН НА ЧИСЛОВОЙ ПРЯМОЙ».
+//ДАННЫЕ КЛАССА : ЛЕВАЯ ГРАНИЦА ДИАПАЗОНА, ПРАВАЯ ГРАНИЦА ДИАПАЗОНА.
+//ФУНКЦИИ КЛАССА : СРАВНЕНИЕ ДВУХ ДИАПАЗОНОВ, НАХОЖДЕНИЕ ДЛИНЫ ДИАПАЗОНА, 
+//ПРОВЕРКА, ПОПАДАЕТ ЛИ НЕКОТОРОЕ ЧИСЛО В ДИАПАЗОН, ПРОВЕРКА, ПЕРЕСЕКАЮТСЯ ЛИ ДИАПАЗОНЫ.
